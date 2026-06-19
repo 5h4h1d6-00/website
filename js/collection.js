a5f4d06fe5c7a7b01e60b371f1337843ddcf5d19
5|-|4|-|1d <shahidsaleemitoo@gmail.com>
2026-06-19 16:48:13 +0530

    Add functionality to display collection name from URL

--- js/collection.js
+++ js/collection.js
@@ -1,1 +1,13 @@
 
+const params =
+new URLSearchParams(
+location.search
+);
+
+const collectionName =
+params.get("name");
+
+document.getElementById(
+"collectionTitle"
+).textContent =
+collectionName;
