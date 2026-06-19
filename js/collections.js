0dafac7f98622d154aabc3c5213727051c1f565e
5|-|4|-|1d <shahidsaleemitoo@gmail.com>
2026-06-19 16:36:17 +0530

    Update collections.js

--- js/collections.js
+++ js/collections.js
@@ -1,1 +1,36 @@
 
+let collections =
+JSON.parse(
+localStorage.getItem("collections")
+) || ["Default"];
+
+const container =
+document.getElementById(
+"collectionsContainer"
+);
+
+function renderCollections(){
+
+container.innerHTML = "";
+
+collections.forEach(name=>{
+
+const card =
+document.createElement("div");
+
+card.className =
+"collection-card";
+
+card.innerHTML = `
+
+<h3>${name}</h3>
+
+`;
+
+container.appendChild(card);
+
+});
+
+}
+
+renderCollections();
